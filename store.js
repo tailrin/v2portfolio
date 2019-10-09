ffea90aa51d943ee386a5d37b7e7f5a3f9b5b313
tailrin <51151455+tailrin@users.noreply.github.com>
2019-10-08 19:45:42 -0600

    Update store.js

--- store.js
+++ store.js
@@ -20,7 +20,7 @@ function projectGenerator(screenshot, Name, Live, Repo, Tech, Description){
   
       generateProjectHTML: function(){
         const arr = [];
-        arr.push(`<li class="flexible"> <h3>${this.name}</h3>, <img class="screen-shot" src="images/${this.screenShot}" alt="application screenshot">
+        arr.push(`<li class="flexible"> <h3>${this.name}</h3> <img class="screen-shot" src="images/${this.screenShot}" alt="application screenshot">
         <div>`, `<div class="link-wrapper">`, `<a class="white" href="${this.live}" target="_blank"> Live App</a>`, `<a class="white" href="${this.repo}" target="_blank" id="repo">Repo</a>`, `</div>`, `<p>${this.description}</p>`, `<h4>Technologies Used</h4>`, `<ul class="tech">`, this.generateTechList(), `</ul>`, `</div>`, `</li>`);
         return arr.join("");
       }
